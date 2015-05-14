4ad72ffcdc561fe1373b0a7e4108941a7f03eecb
andreisohan <andreisohan@gmail.com>
2015-05-15 02:54:37 +0300

    Exercises day 3 (incomplete)

--- Day3/scripts/controllers/MenuController.js
+++ Day3/scripts/controllers/MenuController.js
@@ -4,6 +4,8 @@ hrApp.controller('MenuController', ['$scope', 'employeeActionsService', function
         label:'Employee List'
     }];
 
+    $scope.employeeActionList = employeeActionsService;
+
     // TODO #12 - load menu items from Value
 
     $scope.currentDate = new Date();
